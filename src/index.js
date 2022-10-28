ab309addbf28f1d5c6707d962f50e580eb3fea1e
mpollachek <mpollachek81@gmail.com>
2022-10-28 16:38:20 -0400

    testing vercel

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='www.allworkersunion.com'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
