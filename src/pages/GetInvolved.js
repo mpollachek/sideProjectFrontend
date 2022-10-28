d9483667b45515e131d26ee387f54f520e81fc82
mpollachek <mpollachek81@gmail.com>
2022-10-28 18:33:44 -0400

    testing frontend communication with backend

--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -15,12 +15,12 @@ import { Link } from 'react-router-dom';
 import {useEffect, useState} from 'react';
 import uprising from '../assets/daumier-the-uprising-1860.jpg';
 import Axios from "axios";
-import backend from '../config';
+//import backend from '../config';
 
 
 const GetInvolved = () => {
 
-  //const backend = process.env.backendString;
+  const backend = process.env.backendString;
 
   const handleSubmit = (values) => {
     console.log('form values: ', values);
