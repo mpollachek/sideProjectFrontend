36435ea22235d503afdd3f1367175b7f7760d919
mpollachek <mpollachek81@gmail.com>
2022-11-04 11:20:33 -0400

    additions to donate

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -4,14 +4,25 @@ import {
   Col, 
 } from "reactstrap";
 import "./Donate.css"
+import business from '../assets/business.png';
+import businessIdea from '../assets/business-idea.jpg';
+import businessIdeaCropped from '../assets/business-idea-cropped.jpg';
+import Technology from '../assets/technology.jpg';
+import businessPeople from '../assets/business-people.jpg';
 
 
 const Donate = () => {
 
   return (
-    <Container>
+    <Container className="full-width">
+      <Row       
+        style={{
+          backgroundImage: `url(${businessIdeaCropped})`,
+          backgroundSize: "cover",
+        }}
+      >
       <Row className="my-4">
-        <Col className="text-center bannerDonate">
+        <Col className="text-center bannerDonateOrg offset-md-1 col-md-10">
           <h1>To successfully fight for change we need funding</h1>
           <h2>All Workers Union is a registered 501(c)4 non-profit organization focused on promoting social welfare and shifting the political landscape back on the side of workers</h2>
           <h2>Donations will be used for outreach, organizing events, spreading our message and operations costs</h2>
@@ -19,9 +30,19 @@ const Donate = () => {
         </Col>
       </Row>
 
+      <Row className="my-4">
+        <Col className="text-center bannerDonateGrn offset-md-1 col-md-10">
+          <h1>We are dedicated to making the changes outlined on the homepage</h1>
+          <h2>Have no illusions, we will be fighting an uphill battle</h2>
+          <h2>For the sake of younger generations, doing nothing is not an option</h2>
+          <h2>With proper funding we will be able to create a movement that sets the course of America back in the direction of prosperity for <b>ALL</b></h2>
+        </Col>
+      </Row>
+      </Row>
+
       {/* donation information */}
 
-      
+
 
       {/* <Row className="my-4">
         <Col className="text-center bannerDonate">
