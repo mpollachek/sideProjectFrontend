ceb841b62a72827cc72e654d8cbd8ad07d8a6a43
mpollachek <mpollachek81@gmail.com>
2022-11-06 19:08:46 -0500

    basic auth testing

--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -48,7 +48,7 @@ const GetInvolved = () => {
     Axios.get(`https://sore-pink-cow-sari.cyclic.app/api/getEmails`, {
       headers: {
         "Content-Type": "application/json",
-        'Authorization': `Basic ${Buffer.from(`${u}:${p}`).toString("base64")}`,
+        // 'Authorization': `Basic ${Buffer.from(`${u}:${p}`).toString("base64")}`,
       },
       withCredentials: true,
     }).then((response) => {
